0688d9f343dad406ccba0b22bcd8c1fac59ca463
RCO04 <rodrigogameelcrack@gmail.com>
2024-06-02 12:18:35 +0200

    Retoque visual

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -17,11 +17,6 @@ const AudioPlayer = ({ src }) => {
         audioRef.current.pause();
     };
 
-    const stopAudio = () => {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-    };
-
     return (
         <div className="p-4 bg-gray-100 rounded-md">
             <audio ref={audioRef} className="w-full">
@@ -31,7 +26,6 @@ const AudioPlayer = ({ src }) => {
             <div className="mt-4 flex justify-center space-x-4">
                 <button onClick={playAudio} className="px-4 py-2 bg-blue-500 text-white rounded-md">Play</button>
                 <button onClick={pauseAudio} className="px-4 py-2 bg-yellow-500 text-white rounded-md">Pause</button>
-                <button onClick={stopAudio} className="px-4 py-2 bg-red-500 text-white rounded-md">Stop</button>
             </div>
         </div>
     );
