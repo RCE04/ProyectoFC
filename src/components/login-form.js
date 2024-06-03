5a01c5f7c3fe49919d397e47d72d50445a18c586
RCO04 <rodrigogameelcrack@gmail.com>
2024-06-03 09:05:38 +0200

    Retoque visual de la pagina de incio de sesion y registrarse

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,38 +1,40 @@
-'use client';
+'use client'
 import { useState } from 'react';
-import { login } from '@/lib/actions';
+import { login } from '@/lib/actions'
 import Button from '@/components/button-form';
 
 function LoginForm() {
-    const [resultado, setResultado] = useState("");
-    const [tipo, setTipo] = useState("");
+    const [resultado, setResultado] = useState("")
+    const [tipo, setTipo] = useState("")
 
     async function wrapper(data) {
-        const message = await login(data); // Server action
+        const message = await login(data) // Server action
         if (message?.success) {
             // setTipo('success')
             // setResultado(message.success);
         }
         if (message?.error) {
-            setTipo('error');
+            setTipo('error')
             setResultado(message.error);
         }
-    }
 
+    }
     return (
-        <form onSubmit={wrapper} className='flex flex-col items-center justify-center w-full max-w-md mx-auto space-y-4 mt-8 p-4 bg-gray-100 rounded-lg'>
+        <form action={wrapper} className='flex flex-col items-center justify-center w-full max-w-md mx-auto space-y-4 mt-8 p-4 bg-gray-100 rounded-lg shadow-md'>
             <div className='w-full'>
-                <label htmlFor='email' className='block mb-1 font-semibold'>Email</label>
-                <input type='email' id='email' name='email' placeholder="algo@gmail.com" className='w-full py-2 px-4 mb-4 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500' />
+                <label className='block mb-2 font-semibold'>Email
+                    <input type='email' name='email' placeholder="algo@gmail.com" className='w-full py-2 px-4 mt-1 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-transform duration-300 transform hover:scale-105' />
+                </label>
             </div>
             <div className='w-full'>
-                <label htmlFor='password' className='block mb-1 font-semibold'>Contraseña</label>
-                <input type='password' id='password' name='password' placeholder="******" className='w-full py-2 px-4 mb-4 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500' />
+                <label className='block mb-2 font-semibold'>Contraseña
+                    <input type="password" name='password' placeholder="******" className='w-full py-2 px-4 mt-1 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-transform duration-300 transform hover:scale-105' />
+                </label>
             </div>
-            {tipo && <p className={`text-sm text-${tipo}-500`}>{resultado}</p>}
+            <p className={`info ${tipo} text-sm ${tipo === 'error' ? 'text-red-500' : 'text-green-500'}`}>{resultado}</p>
             <Button title="Iniciar sesión" />
         </form>
     );
-}
+};
 
 export default LoginForm;
--- src/components/register-form.js
+++ src/components/register-form.js
@@ -22,23 +22,25 @@ function RegisterForm() {
 
     }
     return (
-        <form action={wrapper} className='credentials'>
-            <div>
-                <label>Nombre
-                    <input type='text' name='nombre' placeholder="Pepe Garcia Ruiz" />
+        <form action={wrapper} className='flex flex-col items-center justify-center w-full max-w-md mx-auto space-y-4 mt-8 p-4 bg-gray-100 rounded-lg shadow-md'>
+            <div className='w-full'>
+                <label className='block mb-2 font-semibold'>Nombre
+                    <input type='text' name='nombre' placeholder="Pepe Garcia Ruiz" className='w-full py-2 px-4 mt-1 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-transform duration-300 transform hover:scale-105' />
                 </label>
-                <label>Email
-                    <input type='email' name='email' placeholder="jose@mail.com" />
-                </label>
-                <label>Contraseña
-                    <input type="password" name='password' placeholder="******" />
-                </label>
-                <p className={`info ${tipo}`}> {resultado} </p>
             </div>
-
+            <div className='w-full'>
+                <label className='block mb-2 font-semibold'>Email
+                    <input type='email' name='email' placeholder="jose@mail.com" className='w-full py-2 px-4 mt-1 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-transform duration-300 transform hover:scale-105' />
+                </label>
+            </div>
+            <div className='w-full'>
+                <label className='block mb-2 font-semibold'>Contraseña
+                    <input type="password" name='password' placeholder="******" className='w-full py-2 px-4 mt-1 bg-white border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 transition-transform duration-300 transform hover:scale-105' />
+                </label>
+            </div>
+            <p className={`info ${tipo} text-sm ${tipo === 'error' ? 'text-red-500' : 'text-green-500'}`}>{resultado}</p>
             <Button title="Crear cuenta" />
         </form>
-
     );
 };
 
